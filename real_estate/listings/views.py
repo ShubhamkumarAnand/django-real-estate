c1cc22a8d716f3f32707bfc9e51859c5f0dcb3e3
imskanand <the.anand3099@gmail.com>
2023-01-07 01:05:00 +0530

    created a view for list all the listing in the DB

--- real_estate/listings/views.py
+++ real_estate/listings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+def listings_list(request):
+    listings = models.Listing.objects.all()
+    context = {
+        "listings": listings
+    }
+    return render(request, 'listings.html', context)
