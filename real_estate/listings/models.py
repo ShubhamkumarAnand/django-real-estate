8979d3da6a50fbebf49ecd889b05c865f99d9585
imskanand <the.anand3099@gmail.com>
2023-01-07 13:30:16 +0530

    Added an Image Field in the models to take input image Make the migration

--- real_estate/listings/models.py
+++ real_estate/listings/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
 class Listing(models.Model):
     title = models.CharField(max_length=100)
-    price= models.IntegerField()
+    price = models.IntegerField()
     num_bedrooms = models.IntegerField()
     num_bathrooms = models.IntegerField()
     square_footage = models.IntegerField()
     address = models.CharField(max_length=255)
-    # image
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
